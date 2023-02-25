e39ed009e51d98aa33eca42a8f62b3f48e93a9f1
toluwanimia <toluwanimia@gmail.com>
2023-02-25 17:02:57 +0100

    deployment replace localhost

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(cors())
 app.use(xss())
 app.use(mongoSanitize())
 
-app.use(morgan('tiny'))
+
 app.use(express.json())
 app.use(cookieParser(process.env.JWT_SECRET))
 // for file upload
@@ -54,14 +54,7 @@ app.use(express.static('./public'));
 app.use(fileUpload())
 
 
-app.get('/', (req, res) => {
-    res.send('e-commerce API')
-})
-app.get('/api/v1', (req, res) => {
-    // console.log(req.cookies)
-    console.log(req.signedCookies)
-    res.send('e-commerce API')
-})
+
 // routes
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
